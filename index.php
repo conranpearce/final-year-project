38086af16a9e45490fecf8663f88c830b052fb66
conranpearce <54678624+conranpearce@users.noreply.github.com>
2020-10-13 11:36:23 +0100

    added php api call

--- index.php
+++ index.php
@@ -10,6 +10,54 @@
         ?>
 
         <h1>This is an introduction</h1>
+
+        <?php
+            $curl = curl_init();
+
+            curl_setopt_array($curl, array(
+            CURLOPT_URL => "https://api.carbonintensity.org.uk/intensity/",
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => "",
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => "GET",
+            ));
+
+            $response = curl_exec($curl);
+
+            curl_close($curl);
+            // echo $response;
+
+            $decoded = json_decode($response, true);
+            echo "<h2>" . $decoded['data'][0]['intensity']['index'] . "</h2>";
+        ?>
+
+        <!-- Testing calling javascript -->
+        <script>
+            function callAPI() {
+                var requestOptions = {
+                method: 'GET',
+                redirect: 'follow'
+                };
+
+                fetch("https://api.carbonintensity.org.uk/intensity/", requestOptions)
+                    .then(response => response.json())
+                    .then((result) => {
+                        console.log(result);
+                        console.log(result['data'][0]['intensity']['index']);
+                        return result;
+                    })
+                    .catch(error => console.log('error', error));
+            }
+        </script>
+
+        <!-- Calling javascript function above-->
+        <script type="text/javascript"> 
+            callAPI();
+        </script>
+
     </section>
 
 <?php
