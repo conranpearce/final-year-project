8db41d328ff6efd0f72a189d595a546652d09c06
conranpearce <54678624+conranpearce@users.noreply.github.com>
2020-10-16 09:02:25 +0100

    added checkbox after returning device list with credentials

--- index.php
+++ index.php
@@ -12,6 +12,9 @@
         <h1>This is an introduction</h1>
 
         <?php
+
+            // Get current carbon intensity
+
             $curl = curl_init();
 
             curl_setopt_array($curl, array(
@@ -28,10 +31,116 @@
             $response = curl_exec($curl);
 
             curl_close($curl);
+            // Not print out full carbon intensity data
             // echo $response;
 
             $decoded = json_decode($response, true);
             echo "<h2>" . $decoded['data'][0]['intensity']['index'] . "</h2>";
+
+            // Getting UUID4
+            $curl = curl_init();
+
+            curl_setopt_array($curl, array(
+            CURLOPT_URL => "https://www.uuidgenerator.net/api/version4",
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => "",
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => "GET",
+            ));
+
+            $response = curl_exec($curl);
+
+            $uuid4 = $response;
+
+            curl_close($curl);
+            // Not printing out uuid
+            // echo "<p>" . $uuid4 . "</p>";
+        
+            // Get token from TP-Link using log in credentials
+
+            $curl = curl_init();
+
+            curl_setopt_array($curl, array(
+            CURLOPT_URL => "https://wap.tplinkcloud.com",
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => "",
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => "POST",
+            CURLOPT_POSTFIELDS =>"{\n \"method\": \"login\",\n \"params\": {\n \"appType\": \"Kasa_Android\",\n \"cloudUserName\": \"cameron.backus@gmail.com\",\n \"cloudPassword\": \"jes5&SW2Mq5&qPcvGZba\",\n \"terminalUUID\": \"$uuid4\"\n }\n}",
+            CURLOPT_HTTPHEADER => array(
+                "Content-Type: text/plain"
+            ),
+            ));
+
+            $response = curl_exec($curl);
+
+            curl_close($curl);
+            // echo $response;
+
+            $decoded = json_decode($response, true);
+            // echo $decoded['result']['token'];
+
+            $token = $decoded['result']['token'];
+
+            // Get device list using token generated from before
+
+            $curl = curl_init();
+
+            curl_setopt_array($curl, array(
+            CURLOPT_URL => "https://wap.tplinkcloud.com",
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => "",
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => "POST",
+            CURLOPT_POSTFIELDS =>"{\n \"method\": \"getDeviceList\",\n \"params\": {\n \"token\": \"$token\"\n }\n}",
+            CURLOPT_HTTPHEADER => array(
+                "Content-Type: text/plain"
+            ),
+            ));
+
+            $response = curl_exec($curl);
+
+            curl_close($curl);
+            echo $response;
+
+            $decoded = json_decode($response, true);
+
+            $plugAlias = $decoded['result']['deviceList'][0]['alias'];
+
+            // echo $plugAlias
+
+            // $length = json_decode($decoded['result']['deviceList'], true);
+
+            $elementCount = count($decoded['result']['deviceList']);
+
+            for ($x = 0; $x < $elementCount; $x++) {
+                echo "<p>" . $decoded['result']['deviceList'][$x]['alias'] . "</p>";
+
+                if ($decoded['result']['deviceList'][$x]['status'] == 0) {
+                        
+                    str_replace(' ', '', );
+
+                    echo "<label class='switch'>
+                            <input type='checkbox' id='" . str_replace(' ', '', strtolower($decoded['result']['deviceList'][$x]['alias'])) . "' onclick='handleClick(this, this.id);'>
+                            <span class='slider round'></span>
+                        </label>";
+            
+                } else if ($decoded['result']['deviceList'][$x]['status'] == 1) {
+                    echo "<label class='switch'>
+                            <input type='checkbox' id='" . str_replace(' ', '', strtolower($decoded['result']['deviceList'][$x]['alias'])) . "'checked onclick='handleClick(this, this.id);'>
+                            <span class='slider round'></span>
+                        </label>";
+                }
+            }
         ?>
 
         <!-- Testing calling javascript -->
@@ -59,6 +168,8 @@
         </script>
 
     </section>
+
+<script src="js/checkbox.js"></script>
 
 <?php
     include_once 'footer.php';
---
+++ js/checkbox.js
@@ -0,0 +1,14 @@
+function handleClick(cb, id) {
+    // display("Click, new value = " + cb.checked, id);
+
+    console.log(cb.checked);
+    console.log(id);
+}
+//   function display(msg) {
+//     // var p = document.createElement('p');
+//     // p.innerHTML = msg;
+//     // document.body.appendChild(p);
+
+//     console.log(msg);
+//     console.log(id);
+//   }
